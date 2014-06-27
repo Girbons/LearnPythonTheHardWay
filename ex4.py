2e730bfc0f9452a44c3ba8187e5801d472f0fcfc
ALessandro <alessandrodea22@gmail.com>
2014-06-27 12:45:38 +0200

    more variables ann printing

---
+++ ex4.py
@@ -0,0 +1,17 @@
+my_name='Alessandro'
+my_age=16
+my_height=183#cm
+my_weight=74#kg
+my_eyes='black or brown'#i don't know
+my_hair='black'
+my_teeth='white'
+
+print "let'stalk about %s"%my_name
+print "he's %d inches tall"%my_height
+print "he's %d pounds heavy"%my_weight
+print "actually thats's not too heavy"
+print "he's got %s eyes and%s hait"% (my_eyes,my_hair)
+print "hi teeth are usally %s depending on the coffe"
+
+#this line is tricky to get it exactly right
+print "if i add %d,%d, and %d i get %d."%(my_age,my_height,my_weight,my_age+my_height+my_weight)
